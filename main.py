6f97aa28f8deddbb0de9db6696bbc31bf4f3f45f
James Espinosa <psyje12@nottingham.ac.uk>
2026-06-10 02:38:27 +0100

    main

--- main.py
+++ main.py
@@ -5,6 +5,18 @@ import menu
 
 pygame.init()
 
-screen = pygame.display.set_mode((settings.WIDTH, settings.HEIGHT, pygame.RESIZABLE)) # screen for sudoku
+screen = pygame.display.set_mode(
+    (settings.WIDTH, settings.HEIGHT),
+    pygame.RESIZABLE) # screen for sudoku
 
 menu.draw_menu(screen)
+
+def main():
+    game = Game(screen)
+
+    while game.running:
+        pygame.settings.FPS=60
+    
+    pygame.quit()
+
+main()
