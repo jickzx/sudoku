5171f40af6154b16d44b1c551241e0a70d559835
James Espinosa <psyje12@nottingham.ac.uk>
2026-06-10 02:06:07 +0100

    additions

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 import pygame
+import sys # interacts directly with the interpreter
+import settings
+import menu
+
+pygame.init()
+
+screen = pygame.display.set_mode((settings.WIDTH, settings.HEIGHT, pygame.RESIZABLE)) # screen for sudoku
+
+menu.draw_menu(screen)
--- settings.py
+++ settings.py
@@ -1,1 +1,17 @@
 import pygame
+
+# window settings
+WIDTH = 600
+HEIGHT = 700
+FPS = 60
+TITLE = "Sudoku"
+
+
+# board settings (should only be 2x2 and 3x3 for now, maybe only work on 3x3)
+
+# colours (can add functionality to this later)
+
+# font sizes
+
+# game rules
+
