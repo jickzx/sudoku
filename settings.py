2573d63d5030116dd1667e32c6ce7fcfe07d5d93
James Espinosa <psyje12@nottingham.ac.uk>
2026-06-10 02:07:49 +0100

    added colours

--- settings.py
+++ settings.py
@@ -10,6 +10,10 @@ TITLE = "Sudoku"
 # board settings (should only be 2x2 and 3x3 for now, maybe only work on 3x3)
 
 # colours (can add functionality to this later)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GRAY = (200, 200, 200)
+
 
 # font sizes
 
